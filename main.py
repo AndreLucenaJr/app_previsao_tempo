03cd46401480fcd1c915768172329ca0052e5210
AndreLucenaJr <andrelucenajr123@gmail.com>
2023-09-22 10:09:26 -0300

    add api

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import requests
 from pprint import pprint
 
-API_key = ''
+API_key = '1a05332c99683ae41d8eb54519e6d058'
 
 #O usuário escolhe a cidade que quer analisar 
 cidade = input('Digite uma cidade: ')
@@ -12,5 +12,5 @@ base_url = 'https://api.openweathermap.org/data/2.5/weather?appid='+ API_key + '
 #tranformando o resposta do base_url em json
 dados_tempo = requests.get(base_url).json()
 
-
+pprint(dados_tempo)
  
